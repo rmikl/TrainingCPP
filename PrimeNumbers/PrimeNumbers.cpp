6fb13c92f9327943465125d6c3dc35462de68afc
robertm131 <robertm131@gmail.com>
2018-10-14 22:42:10 +0200

    fixed part with second loop and comparing to loop with prime numbers

--- PrimeNumbers/PrimeNumbers.cpp
+++ PrimeNumbers/PrimeNumbers.cpp
@@ -5,7 +5,6 @@ using namespace std;
 int main()
 {	
 	int maxNumberToCheck;
-	int counter=0;
 	
 	cout<<"Please give me number to which i'll check prime numbers"<<endl;
 	cin>>maxNumberToCheck;
@@ -19,30 +18,36 @@ int main()
 		if(i == 2){
 			arrayWithPrimeNumbers.AddElement(i);
 		}
-		else{
-			for (int j = 0 ; j < arrayWithPrimeNumbers.GetNumberOfElements() ; j++)
+		else
+		{
+			int counter ;
+			counter =0 ;
+			for (int j  = 0 ; j < arrayWithPrimeNumbers.GetNumberOfElements() ; j++)
 			{
-	//			cout << "number before if i: "<< i << endl;
-	//			cout << "number before if array[j]: " << arrayWithPrimeNumbers.GetElementWithIndex(j) << endl;
+		//		cout << "number before if i: "<< i << endl;
+		//		cout << "number before if array[j]: " << arrayWithPrimeNumbers.GetElementWithIndex(j) << endl;
 				int modulo = i % arrayWithPrimeNumbers.GetElementWithIndex(j);
-	//			cout << "value of modulo: " << modulo << endl;
+		//		cout << "value of modulo: " << modulo << endl;
 
-				if(modulo == 1){
-					counter++;											
-				}else if(modulo == 0){
-					i++;
-				}
-
-				if(counter > 0){
-	//				cout << "counter: " << counter << endl;
-					arrayWithPrimeNumbers.AddElement(i);
-					counter = 0;
+				if(modulo == 1)
+				{
+					continue;
+				}else if(modulo == 0)
+				{
+					counter++;
 					break;
 				}
 
 			}
+			if(counter == 0)
+			{
+		//		cout << "counter: " << counter << endl;
+				arrayWithPrimeNumbers.AddElement(i);
+				counter = 0;
+			}
 		}
 	}
+	
 //	cout<<"Size After: "<<arrayWithPrimeNumbers.GetNumberOfElements()<<endl;
 	cout << "list of prime numbers: "<< endl;
 	for (int i = 0 ; i < arrayWithPrimeNumbers.GetNumberOfElements(); i++)
