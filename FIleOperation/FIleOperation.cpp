c5bcafbdee81fb82fbfcaa00f7894b9443f228d7
robertm131 <robertm131@gmail.com>
2018-10-22 10:10:17 +0200

    added FileOperation project

---
+++ FIleOperation/FIleOperation.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<algorithm>
+#include "../Classes/Array.h"
+
+using namespace std;
+
+
+bool isNumber(string str)
+{
+    for (int i = 0 ; i < str.length()-1; i++)
+    {
+        if(str.find_first_not_of("0123456789"))
+        {
+            return false;
+        }
+        else return true;
+    }
+}
+
+//program that is reading only number from given files and store it in Array object defined in ../Classes/Array.h
+int main()
+{
+
+    string location_of_file;
+    ifstream file;
+    cout<<"Give me the name of the file that you store "<<endl;
+    cin>> location_of_file;
+    file.open(location_of_file);
+    string content_of_file;
+    
+    if(file.is_open())
+    {
+        while(!file.eof()){
+            string tmp;            
+            file >> tmp;
+            if(!isNumber(tmp)){   
+
+                tmp += '\n';
+                content_of_file += tmp;
+            }
+        }
+    }
+    file.close();
+    Array arr;
+
+    cout << content_of_file << endl;
+    for (int i = 0; i < content_of_file.length()-2 ; i+=2){
+        int n = atoi(&content_of_file[i]);
+        arr.AddElement(n);
+    }
+
+    cout  << "Content of created array from elements given from file: " << endl;
+
+    arr.ShowArray();
+
+    return 0;
+}
